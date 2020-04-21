525e7bd023aafc6259559b3ff830e98bd2738066
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-04-21 19:19:20 +0200

    refactor(verif): move verifier stuff for bind to work right

--- src/sdk.ts
+++ src/sdk.ts
@@ -11,19 +11,56 @@ export const sdkPlugin: Plugin<SDKOptions> = {
   register: async (server: Server, options: SDKOptions) => {
     const identity = await JolocomSDK.fromMnemonic(options.mnemonic);
 
-    const bind = {
-      identity,
-    };
     // const prefix = options.prefix;
 
-    server.bind(bind);
-
-    options.verifierOptions.map(
-      async (verificationOptions) =>
-        await server.register({
-          plugin: verifierPlugin,
-          options: verificationOptions,
-        })
-    );
+    if (options.verifierOptions)
+      options.verifierOptions.map((opts) => {
+        const path = `/${opts.name || opts.requirements[0].type[0]}/`;
+        server.route({
+          method: "GET",
+          path,
+          handler: verificationRequestHandler,
+          options: {
+            bind: {
+              identity,
+              requirements: opts.requirements,
+            },
+          },
+        });
+        server.route({
+          method: "POST",
+          path,
+          handler: verificationResponseHandler,
+          options: {
+            bind: {
+              identity,
+              requirements: opts.requirements,
+            },
+          },
+        });
+      });
   },
 };
+
+async function verificationRequestHandler(
+  request: Request,
+  h: ResponseToolkit
+) {
+  const callbackUrl = request.url.href;
+  return await h.context.identity.credRequestToken({
+    callbackUrl,
+    credentialRequirements: h.context.requirements,
+  });
+}
+
+async function verificationResponseHandler(
+  request: Request,
+  h: ResponseToolkit
+) {
+  const p = request.payload as { token: string };
+  if (!p.token) return false;
+
+  const token: string = p.token;
+  const res = await h.context.identity.tokenReceived(token);
+  return res;
+}
